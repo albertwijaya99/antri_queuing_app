8c74ca2165be0b676d282998834aff58b0c59392
Albert <albetz99@gmail.com>
2020-11-07 12:52:50 +0700

    inital commit & setup firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAfKbOj9aiATuQPWRL--21wdYCkAPVWLgg',
+    authDomain: 'antri-45436.firebaseapp.com',
+    databaseURL: 'https://antri-45436.firebaseio.com',
+    projectId: 'antri-45436',
+    storageBucket: 'antri-45436.appspot.com',
+    messagingSenderId: '284723073812',
+    appId: '1:284723073812:web:5ee466282bfa7481992801',
+    measurementId: 'G-5N9MGYSC3H'
+  }
 };
 
 /*
